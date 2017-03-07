637eeb4660f5f4cebecf41eae9dc33dd23980c27
jnyiok <jnyiok@unomaha.edu>
2017-03-07 12:01:37 -0600

    testapp

---
+++ app/src/main/java/com/lenovolap/android/testapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lenovolap.android.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
